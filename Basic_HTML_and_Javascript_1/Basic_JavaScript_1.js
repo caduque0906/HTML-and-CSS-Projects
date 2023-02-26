92482cd075ff0aa0528f371c3a77dbf1db16cd0d
caduque0906 <caduque0906@gmail.com>
2023-02-26 18:28:52 -0500

    fixed .js file

--- Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -1,9 +1,7 @@
-<script>
-	alert("Hello, World!");
-	function My_First_Function() {
-		var String = "Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
-</script>
+alert("Hello, World!");
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
